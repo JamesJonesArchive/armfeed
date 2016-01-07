88fc35d799d168d82a07fb3ac719d457cebfe866
Chance Gray <chance@USF-Gold-Wifi-226-72-210.laptops.usf.edu>
2016-01-07 09:45:36 -0500

    added sample config file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # armfeed
+armfeed_config.groovy - sample config belongs in /usr/local/etc/idm_config
---
+++ armfeed_config.groovy
@@ -0,0 +1,12 @@
+source {
+	dbConnectString = ''
+	dbUser = ''
+	dbPassword = ''
+	dbDriver = ''
+}
+wsConvertURLPrefix = ""
+wsConvertURLSuffix = ""
+
+accountsQuery = "" as String
+rolesQuery = "" as String
+relDefQuery = "" as String
